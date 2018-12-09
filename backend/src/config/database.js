9af336b3ede8539cc5a4f680ff51a6eff09a9674
Adriano Faria Alves <adriano.faria@outlook.com.br>
2018-12-09 15:53:35 -0200

    Eliminando mensagems de warning

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-module.exports = mongoose.connect("mongodb://localhost/financial");
+mongoose.set("useFindAndModify", false);
+
+module.exports = mongoose.connect(
+  "mongodb://localhost/financial",
+  { useNewUrlParser: true }
+);
+
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
 mongoose.Error.messages.Number.min =
   "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'.";
