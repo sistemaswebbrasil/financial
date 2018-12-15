2059f04e312875119ed9c50cb5b0f75f749ab5c1
Adriano Faria Alves <adriano.faria@outlook.com.br>
2018-12-15 18:23:32 -0200

    Componentizando o formulário

--- frontend/src/billingCycle/BillingCycleForm.js
+++ frontend/src/billingCycle/BillingCycleForm.js
@@ -1,22 +1,63 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { init } from "./billingCycleActions";
+import LabelAndInput from "../common/form/LabelAndInput";
+
 class BillingCycleForm extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
       <form role="form" onSubmit={handleSubmit}>
         <div className="box-body">
-          <Field name="name" component="input" />
-          <Field name="month" component="input" />
-          <Field name="year" component="input" />
+          <Field
+            name="name"
+            component={LabelAndInput}
+            label="Nome"
+            cols="12 4"
+            placeholder="Informe o nome"
+          />
+          <Field
+            name="month"
+            component={LabelAndInput}
+            type="number"
+            label="Mês"
+            cols="12 4"
+            placeholder="Informe o mês"
+          />
+          <Field
+            name="year"
+            component={LabelAndInput}
+            type="number"
+            label="Ano"
+            cols="12 4"
+            placeholder="Informe o ano"
+          />
         </div>
         <div className="box-footer">
           <button type="submit" className="btn btn-primary">
             Submit
+          </button>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={this.props.init}
+          >
+            Cancelar
           </button>
         </div>
       </form>
     );
   }
 }
-export default reduxForm({ form: "billingCycleForm" })(BillingCycleForm);
+
+BillingCycleForm = reduxForm({
+  form: "billingCycleForm",
+  destroyOnUnmount: false
+})(BillingCycleForm);
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
+export default connect(
+  null,
+  mapDispatchToProps
+)(BillingCycleForm);
