2f2f3deb68f694ebc621e98dbfaaceb0ac4fdf76
Adriano Faria Alves <adriano.faria@outlook.com.br>
2019-01-12 18:01:54 -0200

    Autenticação no servidor

--- src/backend/config/cors.js
+++ src/backend/config/cors.js
@@ -2,11 +2,11 @@ module.exports = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH,DELETE"
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With,Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 };
